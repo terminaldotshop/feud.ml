59b444548b194205946cdf796b0fc437467d514b
adamelmore <2363879+adamdottv@users.noreply.github.com>
2024-11-17 19:12:33 -0600

    feat: styles

--- src/bin/adam.tsx
+++ src/bin/adam.tsx
@@ -1,16 +1,12 @@
-console.log("[production] Starting Adam Server...")
+console.log("[production] Starting Adam Server...");
 
-import { server } from "../server.jsx"
+import { server } from "../server.jsx";
 
-import * as Bus from "../events.js"
+import * as Bus from "../events.js";
 
-let state = Bus.getState()
-state.running = false
-state.questions = [
-  "This is the first question",
-  "This is the second question",
-]
-
+let state = Bus.getState();
+state.running = true;
+state.questions = ["name something that you do by yourself in your free time?"];
 
 // Log server to make sure that the file is required and loaded...
 console.log("[production] ... completed setup", server);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: [
-		"./index.html",
-		"./src/*.mlx",
-		"./src/components/**/*.{js,jsx,ts,tsx}",
-		"./src/generated/src/**/*.{js,jsx,ts,tsx}",
-		"./vendor/**/*.{js,jsx,ts,tsx}",
-	],
+  content: [
+    "./index.html",
+    "./src/*.mlx",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/generated/src/**/*.{js,jsx,ts,tsx}",
+    "./vendor/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         geist: "Geist Mono, monospace",
-      }
+      },
+      colors: {
+        brand: "#FF5C00",
+        muted: "#969696",
+      },
     },
   },
   plugins: [],
-}
-
+};
